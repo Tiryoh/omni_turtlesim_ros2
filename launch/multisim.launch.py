f5dd0e20f5908b8457f4cea16c2471b98733ff75
Daisuke Sato <tiryoh@gmail.com>
2020-07-08 21:31:14 +0900

    fix: Rename package name in launch (#5)

--- launch/multisim.launch.py
+++ launch/multisim.launch.py
@@ -4,7 +4,7 @@ import launch_ros.actions
 def generate_launch_description():
     return LaunchDescription([
         launch_ros.actions.Node(
-            namespace= "turtlesim1", package='turtlesim', executable='turtlesim_node', output='screen'),
+            namespace= "turtlesim1", package='omni_turtlesim', executable='turtlesim_node', output='screen'),
         launch_ros.actions.Node(
-            namespace= "turtlesim2", package='turtlesim', executable='turtlesim_node', output='screen'),
+            namespace= "turtlesim2", package='omni_turtlesim', executable='turtlesim_node', output='screen'),
     ])
